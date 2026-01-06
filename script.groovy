fcd120700274a6983f175c6c21edf6df3e12ed81
Aws Radaideh <95333190+AwsRad02@users.noreply.github.com>
2026-01-06 15:28:52 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,11 @@ def buildJar(){
 
 def buildImage(){
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId:'dockerhub-rep',passwordVariable:'PASS',usernameVariable:'USER')])
+    withCredentials([usernamePassword(credentialsId:'dockerhub-rep',passwordVariable:'PASS',usernameVariable:'USER')]){
     sh 'docker build -t awsradaideh/my-repo:jma-2.0 . '
     sh "docker login -u $USER -p $PASS"
     sh 'docker  push awsradaideh/my-repo:jma-2.0'
+    }
 }
 
 
